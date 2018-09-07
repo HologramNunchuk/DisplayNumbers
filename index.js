c4850a0c5576ea054e663eac2771894d5b4eae6b
Jaime <jaimesof82@gmail.com>
2018-09-06 21:25:53 -0700

    Same Display Numbers function, but with more concise code using a ternary/conditional operator.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-showNumbers();
+showNumbers(10);
 
 function showNumbers(limit) {
   for (let i = 0; i <= limit; i++) {
-    if (i % 2 === 0) console.log(i, "Even");
-    else console.log(i, "Odd");
+    const message = i % 2 === 0 ? "Even" : "Odd";
+    console.log(i, message);
   }
 }
